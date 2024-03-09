76b5ef72398a2bf2664ea399dbdefcdf3ac657ca
MariamYei <yeilygomez1717p@gmail.com>
2024-03-09 11:20:01 -0500

    parte 2 vid yei

---
+++ UDIE/capaNEGOCIOS/Acciones/AccionesBases.cs
@@ -0,0 +1,9 @@
+﻿using capaDATOS.DataBase;
+
+namespace capaNEGOCIOS
+{
+    public class AccionesBases
+    {
+        public DbLibraryEntityDataContext dblibcontext = new DbLibraryEntityDataContext();
+    }
+}
