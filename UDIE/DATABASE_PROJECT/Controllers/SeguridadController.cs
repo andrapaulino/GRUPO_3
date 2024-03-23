c1ab87e5727dcd5d00f9fb1f943587c5c30790b4
Whitney Astacio <wastacio@est.unibe.edu.do>
2024-03-23 16:35:31 -0400

    Wini

--- UDIE/DATABASE_PROJECT/Controllers/SeguridadController.cs
+++ UDIE/DATABASE_PROJECT/Controllers/SeguridadController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace DATABASE_PROJECT.Controllers
 {
@@ -16,7 +17,24 @@ namespace DATABASE_PROJECT.Controllers
 
         public ActionResult AccederLogin(string usuario = "", string clave = "") 
         {
-            return View(); //retornar al perfil correspondiente (Funcional, Técnico y Administrador)
+            return View({ "_UsuarioAdm"); //retornar al perfil correspondiente (Funcional, Técnico y Administrador)
+        }
+
+
+        public ActionResult _UsuarioFuncional()
+        {
+            return View();
+        }
+
+        public ActionResult _UsuarioTecnico() 
+        {
+        
+            return View();
+        
+        }
+        public ActionResult _UsuarioAdmin()
+        {
+            return View();
         }
     }
 }
